1b984757c23b52bfcc61d5ff9907ac1fed0f5fb9
LuizLimaDev <devluizlima@gmail.com>
2023-10-18 11:37:09 -0300

    Exercicios resolvidos

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -1,21 +1,34 @@
 type Item = {
-    nome: string,
-    descricao: string,
-    valor: number
-}
+  nome: string;
+  descricao: string;
+  valor: number;
+};
 
 type Cartao = {
-    numero: number,
-    validade: string,
-    nome: string,
-    cvv: number
-}
+  numero: number;
+  validade: string;
+  nome: string;
+  cvv: number;
+};
 
 type Carrinho = {
-    item: Item,
-    qtd: number,
-    desconto: number,
-    frete: number,
-    tipoTransacao: 'credito' | 'Debito',
-    cartao: Cartao
-}
+  item: Item;
+  qtd: number;
+  desconto: number;
+  frete: number;
+  tipoTransacao: "credito" | "Debito";
+  cartao: Cartao;
+};
+
+type Endereco = {
+  cep: number;
+  rua: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+};
+
+type NovoCarrinho = Omit<Carrinho, "tipoTransacao"> & {
+  endereco: Endereco;
+  tipoTransacao: "credito" | "débito";
+};
